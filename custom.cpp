770bbc7cd09142eadb0df805b4225221dc176dfb
lixiang <1411667859@qq.com>
2021-04-18 19:23:38 +0800

    。

---
+++ custom.cpp
@@ -0,0 +1,6 @@
+#include "custom.h"
+
+Custom::Custom()
+{
+
+}
---
+++ custom.h
@@ -0,0 +1,12 @@
+#ifndef CUSTOM_H
+#define CUSTOM_H
+
+
+class Custom
+{
+    Q_OBJECT
+public:
+    Custom();
+};
+
+#endif // CUSTOM_H
